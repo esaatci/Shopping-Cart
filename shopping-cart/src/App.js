8b474c26a71b2680cd6161b416e0792df29e8871
Efe Saatci <efesaatci2019@u.northwestern.edu>
2019-02-12 20:03:27 -0600

    closes #8

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -2,24 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class SizeFilter extends React.Component{
+
+  render(){
+    return(
+      <div>
+        size
+        <p>
+          <input type="checkbox" />
+          {' '}
+          X
+          <input type="checkbox" />
+          {' '}
+          XL
+          <input type="checkbox" />
+          {' '}
+          XS
+          <input type="checkbox" />
+          {' '}
+          M
+          <input type="checkbox" />
+          {' '}
+          ML
+        </p>
+      </div>
+    );
+
+  }
+}
+class ProductGrid extends React.Component{
+  render() {
+    const product = this.props.product;
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SizeFilter/>
       </div>
     );
   }
